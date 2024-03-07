1e0b1aa2c377242b630406511aa6c48d7da4c28f
gagep1994 <c0517025@online.camosun.ca>
2024-03-07 13:51:40 -0800

    Initial Commit

---
+++ pracrtuceQuestions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pracrtuceQuestions
+//
+//  Created by Gage on 2024-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pracrtuceQuestions/pracrtuceQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pracrtuceQuestionsApp.swift
+//  pracrtuceQuestions
+//
+//  Created by Gage on 2024-03-07.
+//
+
+import SwiftUI
+
+@main
+struct pracrtuceQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
